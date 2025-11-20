ee9a5c6e867f6adf36c4092103facd0b033b7f4c
Qbix565 <kubakrajkowski10@gmail.com>
2025-11-20 12:07:06 +0100

    update

---
+++ szpontcode/szpont1.js
@@ -0,0 +1,11 @@
+let kolor=prompt(`Podaj kolor:`);
+switch(kolor){
+    case("zielony"):
+    console.log(`Wybrales zielony`);
+    break;
+    case("czerwony"):
+    console.log("masz okres");
+    break;
+    default:
+        console.log("jestes lekko przyglupi");l
+}
