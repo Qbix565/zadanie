536ff71ebf5fd69c61e0eb02f16583ff424632dc
Qbix565 <kubakrajkowski10@gmail.com>
2026-09-15 19:36:39 +0200

    15.09.26

---
+++ phpp/47a/1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zadanie 1 — Utwórz sześć zmiennych różnych typów</title>
+</head>
+<body>
+
+    <?php
+        $imie = "Jakub";
+        $wiek = 17;
+        $klasa = "4G1";
+        $srednia 4,75;
+        $aktywny = true;
+        $liczbaPunkotw = 35;
+
+
+    ?>
+
+</body>
+</html>
